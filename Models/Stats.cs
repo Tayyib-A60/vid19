dc4e2c8354d01d12666151507ed8b659ed9f850b
Toheeb Adesokan <Toheeb Adesokan@DESKTOP-9CELES0>
2021-05-03 12:34:39 +0100

    this is just a message, it is just a commit message but it should describe what has been done

---
+++ Models/Stats.cs
@@ -0,0 +1,13 @@
+using System.Text.Json.Serialization;
+namespace vid19.Models 
+{
+    public class Stats
+    {
+        [JsonPropertyName("TotalConfirmed")]
+        public long Confirmed { get; set; }
+        [JsonPropertyName("TotalDeaths")]
+        public long Deaths  { get; set; }
+        [JsonPropertyName("TotalRecovered")]
+        public long Recovered { get; set; }
+    }
+}
